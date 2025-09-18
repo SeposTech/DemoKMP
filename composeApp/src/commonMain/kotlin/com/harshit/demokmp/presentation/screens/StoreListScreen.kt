5fc2d8e30b9cf3b3216f22682592660b89227cd9
Himanshu Mehra <onlineorderapp2020@gmail.com>
2025-09-18 16:13:16 +0530

    added ios changes

--- composeApp/src/commonMain/kotlin/com/harshit/demokmp/presentation/screens/StoreListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/harshit/demokmp/presentation/screens/StoreListScreen.kt
@@ -1,10 +1,15 @@
 package com.harshit.demokmp.presentation.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import com.harshit.demokmp.interfaces.StoreListHandler
 import com.harshit.demokmp.presentation.screens.viewmodel.StoreListViewModel
@@ -15,11 +20,20 @@ fun StoreListPage(storeListHandler: StoreListHandler) {
 
     val uiState = storeListHandler.storeState.collectAsState()
 
-    when(uiState.value) {
-        is StoreListViewModel.UiState.Loading-> {LoaderPage()}
-        is StoreListViewModel.UiState.Success -> {ShowStoreList()}
+    when (uiState.value) {
+        is StoreListViewModel.UiState.Loading -> {
+            LoaderPage()
+        }
+
+        is StoreListViewModel.UiState.Success -> {
+            ShowStoreList()
+        }
+
         is StoreListViewModel.UiState.Error -> {}
-        is StoreListViewModel.UiState.NoInternet -> {NoInternetPage(onRetry = {})}
+        is StoreListViewModel.UiState.NoInternet -> {
+            NoInternetPage(onRetry = {})
+        }
+
         else -> Unit
     }
 
@@ -34,8 +48,15 @@ fun ShowStoreList() {
 
     Surface(modifier = Modifier.fillMaxSize()) {
 
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
 
+            Text(text = "Store List..", style = MaterialTheme.typography.bodyMedium)
 
+        }
 
 
     }
--- composeApp/src/iosMain/kotlin/com/harshit/demokmp/iosDi/IosModule.kt
+++ composeApp/src/iosMain/kotlin/com/harshit/demokmp/iosDi/IosModule.kt
@@ -2,20 +2,27 @@ package com.harshit.demokmp.iosDi
 
 import com.harshit.demokmp.connectivity.ConnectivityObserver
 import com.harshit.demokmp.data.network.ApiClient
+import com.harshit.demokmp.data.network.repository.StoreListRepository
 import com.harshit.demokmp.data.network.repository.UserRepository
+import com.harshit.demokmp.domain.models.usecase.StoreListUseCase
 import com.harshit.demokmp.domain.models.usecase.UserLoginUseCase
 import com.harshit.demokmp.presentation.screens.viewmodel.LoginViewModel
+import com.harshit.demokmp.presentation.screens.viewmodel.StoreListViewModel
 import org.koin.dsl.module
 
 val iosModule = module{
     single { ApiClient() }
 
     single { UserRepository(get()) }
+    single { StoreListRepository(get()) }
 
     single { UserLoginUseCase(get()) }
+    single { StoreListUseCase(get()) }
 
     single { ConnectivityObserver() }
 
     single { LoginViewModel(get(),get()) }
+
+    single { StoreListViewModel(get()) }
 
 }
